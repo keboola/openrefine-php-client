801c04882b3f5ad6ae722a5f05916954e9390529
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2016-09-24 01:00:45 +0800

    fix(ghpages push): add user info

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -7,5 +7,7 @@ apigen generate -d /docs/ -s ./src/
 cd /docs
 git init
 git add .
+git config --global user.email "$GITHUB_GHPAGES_COMMIT_EMAIL"
+git config --global user.name "$GITHUB_GHPAGES_COMMIT_NAME"
 git commit -m "Deploy to Github Pages"
 git push --force --quiet "https://$GITHUB_TOKEN@$github.com/keboola/openrefine-php-client.git" master:gh-pages > /dev/null 2>&1
