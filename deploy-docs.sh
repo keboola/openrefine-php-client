4e9679fa574e60d3e383fa69a70f35b5aba9f0c2
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2016-09-24 01:34:27 +0800

    fix(travis): build and upload docs from container

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+#
 
 # deploy
-cd ./docs
+cd /docs
 git init
 git add .
 git config --global user.email "$GITHUB_GHPAGES_COMMIT_EMAIL"
 git config --global user.name "$GITHUB_GHPAGES_COMMIT_NAME"
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://$GITHUB_TOKEN@$github.com/keboola/openrefine-php-client.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://$GITHUB_TOKEN@$github.com/$GITHUB_REPO.git" master:gh-pages > /dev/null 2>&1
