8a11ef076807daaba43f126edbcb26702b8b8c22
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2016-09-24 02:04:34 +0800

    fix(travis): github push

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -9,4 +9,4 @@ git add .
 git config --global user.email "$GITHUB_GHPAGES_COMMIT_EMAIL"
 git config --global user.name "$GITHUB_GHPAGES_COMMIT_NAME"
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://$GITHUB_TOKEN@$github.com/$GITHUB_REPO.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://$GITHUB_TOKEN@github.com/$GITNUB_REPO.git" master:gh-pages
