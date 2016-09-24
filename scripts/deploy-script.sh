2f401e842b1a616e031c01bd9b6311e52af2696b
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2016-09-24 16:48:53 +0800

    fix(travis): messing with deploy script

---
+++ scripts/deploy-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker-compose run --rm \
+      -e GITHUB_GHPAGES_COMMIT_EMAIL=$GITHUB_GHPAGES_COMMIT_EMAIL \
+      -e GITHUB_GHPAGES_COMMIT_NAME=$GITHUB_GHPAGES_COMMIT_NAME \
+      -e GITHUB_TOKEN=$GITHUB_TOKEN \
+      -e GITHUB_REPO=$GITHUB_REPO \
+      tests-coverage-report ./scripts/deploy-docs.sh
