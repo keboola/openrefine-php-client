ca414956d1953c93e5c2cd24d7d064b7c1a9419a
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2019-01-08 13:19:16 +0100

    fix: explanatory comment

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -11,5 +11,9 @@ git fetch origin master:master
 git fetch origin --tags
 cp -r /code /sami/code
 cd /sami/code
+
+# using nasty hack to avoid sami error
+# ERROR: "3" @param tags are expected but only "2" found on "Keboola\OpenRefine\Client::__construct" in /sami/code/src/Keboola/OpenRefine/Client.php:33
 php /sami/sami.phar update -vvv --force /sami/code/.sami-config.php || true
+
 mv /sami/code/docs /code/docs
